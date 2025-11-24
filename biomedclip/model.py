ccb87891ffb69e656f2a8e36440204fcb7b99b37
hanan-haider <137984110+hanan-haider@users.noreply.github.com>
2025-11-24 23:58:27 +0500

    update

--- biomedclip/model.py
+++ biomedclip/model.py
@@ -14,8 +14,48 @@ from torch.utils.checkpoint import checkpoint
 
 
 
+@dataclass
+class CLIPVisionCfg:
+    layers: Union[Tuple[int, int, int, int], int] = 12
+    width: int = 768
+    head_width: int = 64
+    mlp_ratio: float = 4.0
+    patch_size: int = 16
+    image_size: Union[Tuple[int, int], int] = 224
+    ls_init_value: Optional[float] = None  # layer scale initial value
+    patch_dropout: float = 0.2  # what fraction of patches to dropout during training (0 would mean disabled and no patches dropped) - 0.5 to 0.75 recommended in the paper for optimal results
+    input_patchnorm: bool = False # whether to use dual patchnorm - would only apply the input layernorm on each patch, as post-layernorm already exist in original clip vit design
+    global_average_pool: bool = False  # whether to global average pool the last embedding layer, instead of using CLS token (https://arxiv.org/abs/2205.01580)
+    attentional_pool: bool = False # whether to use attentional pooler in the last embedding layer
+    n_queries: int = 256 # n_queries for attentional pooler
+    attn_pooler_heads: int = 8 # n heads for attentional_pooling
+    timm_model_name: str = None  # a valid model name overrides layers, width, patch_size
+    timm_model_pretrained: bool = False  # use (imagenet) pretrained weights for named model
+    timm_pool: str = 'avg'  # feature pooling for timm model ('abs_attn', 'rot_attn', 'avg', '')
+    timm_proj: str = 'linear'  # linear projection for timm model output ('linear', 'mlp', '')
+    timm_proj_bias: bool = False  # enable bias final projection
+    timm_drop: float = 0.  # head dropout
+    timm_drop_path: Optional[float] = None  # backbone stochastic depth
 
 
+
+    
+@dataclass
+class CLIPTextCfg:
+    context_length: int = 512
+    vocab_size: int = 30522
+    width: int = 768
+    heads: int = 12
+    layers: int = 12
+    ls_init_value: Optional[float] = None  # layer scale initial value
+    hf_model_name: str = "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract"
+    hf_tokenizer_name: str = "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract"
+    hf_model_pretrained: bool = True
+    proj: str = 'mlp'  # projection type
+    pooler_type: str = 'cls_last_hidden_state_pooler'
+    embed_cls: bool = False
+    pad_id: int = 0
+    output_tokens: bool = False
 
 
 
@@ -29,26 +69,6 @@ def get_cast_dtype(precision: str):
     elif precision == 'fp16':
         cast_dtype = torch.float16
     return cast_dtype
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     
 class CustomTextCLIP(nn.Module):
