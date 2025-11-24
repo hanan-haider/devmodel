9bf30763655b3ef9a5fdd87685f64563bbefb360
hanan-haider <137984110+hanan-haider@users.noreply.github.com>
2025-11-24 21:37:22 +0500

    update

--- biomedclip/clip.py
+++ biomedclip/clip.py
@@ -13,7 +13,7 @@ from .openai import load_openai_model
 
 _MODEL_CONFIG_PATHS = [Path(__file__).parent / f"model_configs/"]
 _MODEL_CONFIGS = {}  # directory (model_name: config) of model architecture configs
-_MODEL_CKPT_PATHS = {'ViT-L-14-336': Path(__file__).parent / "ckpt/ViT-L-14-336px.pt"}
+_MODEL_CKPT_PATHS = {'BiomedCLIP-PubMedBERT-ViT-B-16': Path(__file__).parent / "ckpt/open_clip_pytorch_model.bin"}
 
 
 def _natural_key(string_):
