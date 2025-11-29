4ced81a9d7181a1f80efecca5003874a17a39305
hanan-haider <137984110+hanan-haider@users.noreply.github.com>
2025-11-29 16:39:38 +0500

    update

--- biomedclip/model.py
+++ biomedclip/model.py
@@ -26,8 +26,10 @@ class CLIPVisionCfg:
     patch_size: int = 16
     image_size: Union[Tuple[int, int], int] = 224
     ls_init_value: Optional[float] = None  # layer scale initial value
-    patch_dropout: float = 0.2  # what fraction of patches to dropout during training (0 would mean disabled and no patches dropped) - 0.5 to 0.75 recommended in the paper for optimal results
-    input_patchnorm: bool = False # whether to use dual patchnorm - would only apply the input layernorm on each patch, as post-layernorm already exist in original clip vit design
+
+    patch_dropout: float = 0.05  # what fraction of patches to dropout during training (0 would mean disabled and no patches dropped) - 0.5 to 0.75 recommended in the paper for optimal results
+
+    input_patchnorm: bool = True # whether to use dual patchnorm - would only apply the input layernorm on each patch, as post-layernorm already exist in original clip vit design
     global_average_pool: bool = False  # whether to global average pool the last embedding layer, instead of using CLS token (https://arxiv.org/abs/2205.01580)
     attentional_pool: bool = False # whether to use attentional pooler in the last embedding layer
     n_queries: int = 256 # n_queries for attentional pooler
@@ -37,11 +39,11 @@ class CLIPVisionCfg:
     
     timm_model_name: Optional[str] = None  # a valid model name overrides layers, width, patch_size
     timm_model_pretrained: bool = False  # use (imagenet) pretrained weights for named model
-    timm_pool: str = 'avg'  # feature pooling for timm model ('abs_attn', 'rot_attn', 'avg', '')
+    timm_pool: str = ''  # feature pooling for timm model ('abs_attn', 'rot_attn', 'avg', '')
     timm_proj: str = 'linear'  # linear projection for timm model output ('linear', 'mlp', '')
     timm_proj_bias: bool = False  # enable bias final projection
-    timm_drop: float = 0.  # head dropout
-    timm_drop_path: Optional[float] = None  # backbone stochastic depth
+    timm_drop: float = 0.1  # head dropout
+    timm_drop_path: Optional[float] = 0.1 # backbone stochastic depth
 
 
 @dataclass
